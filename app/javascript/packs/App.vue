f2a41b4c71d247c0f15b02408d1c36a0349ad054
birnamwood <birnamwood19@gmail.com>
2019-10-07 14:02:13 +0900

    本の登録機能追加

--- app/javascript/packs/App.vue
+++ app/javascript/packs/App.vue
@@ -6,8 +6,12 @@
 </template>
 
 <script>
+  import Header from '../components/Header.vue'
   export default {
     name: 'App',
+    components: {
+      Header
+    },
   }
 </script>
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :books, only: [:index]
 
   namespace :api do
-    resources :books, only: [:index, :show]
+    resources :books, only: [:index, :show, :create, :update]
   end
 
 end
