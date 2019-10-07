53e0e4431405abf696df84050f348dc8193c554b
birnamwood <birnamwood19@gmail.com>
2019-10-07 14:04:58 +0900

    [削除機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :books, only: [:index]
 
   namespace :api do
-    resources :books, only: [:index, :show, :create, :update]
+    resources :books, only: [:index, :show, :create, :update, :destroy]
   end
 
 end
